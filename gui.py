2e04f66ac9248d7390d3858c23df6e1a8e31ae86
Patrick Carillo <p.carillo17@googlemail.com>
2026-03-31 22:10:44 +0200

    Working GUI prototype

--- gui.py
+++ gui.py
@@ -5,69 +5,92 @@ from modules.storage import storage_menu
 from modules.network import network_menu
 from modules.subnetting import subnet_menu
 
-#
-root = tk.Tk()
-root.title("IT-Rechner")
-root.geometry("500x500")
 
-notebook = ttk.Notebook(root)
-notebook.pack(expand=True, fill="both")
+class App_window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("IT-Rechner")
+        self.geometry("500x500")
 
-# Create Tabs
-numbers_tab = ttk.Frame(notebook)
-storage_tab = ttk.Frame(notebook)
-network_tab = ttk.Frame(notebook)
-subnet_tab = ttk.Frame(notebook)
+        notebook = ttk.Notebook(self)
+        notebook.pack(expand=True, fill="both")
 
-notebook.add(numbers_tab, text="Numbers")
-notebook.add(storage_tab, text="Storage")
-notebook.add(network_tab, text="Network")
-notebook.add(subnet_tab, text="Subnet")
+        numbers_tab = ttk.Frame(notebook)
+        storage_tab = ttk.Frame(notebook)
+        network_tab = ttk.Frame(notebook)
+        subnet_tab = ttk.Frame(notebook)
 
+        notebook.add(numbers_tab, text="Numbers")
+        notebook.add(storage_tab, text="Storage")
+        notebook.add(network_tab, text="Network")
+        notebook.add(subnet_tab, text="Subnet")
 
-# Label + Entry for Input
-ttk.Label(numbers_tab, text="Number:").grid(row=0, column=0)
-entry = ttk.Entry(numbers_tab)
-entry.grid(row=0, column=1)
-
-# Dropdown menu
-ttk.Label(numbers_tab, text="System:").grid(row=1, column=0)
-combo = ttk.Combobox(numbers_tab, values=["Decimal", "Non-Decimal"])
-combo.grid(row=1, column=1)
-
-ttk.Label(numbers_tab, text="Base").grid(row=2, column=0)
-base_combo = ttk.Combobox(numbers_tab, values=["2", "8", "16"])
-base_combo.grid(row=2, column=1)
+        NumbersTab(numbers_tab)
+        StorageTab(storage_tab)
 
 
-# Button calls function
-def on_click():
-    result_label.config(text="")
-    bin_label.config(text="")
-    oct_label.config(text="")
-    hex_label.config(text="")
-    if combo.get() == "Decimal":
-        value = int(entry.get())
-        bin_label.config(text=f"Binär: {from_decimal(value, 2)}")
-        oct_label.config(text=f"Oktal: {from_decimal(value, 8)}")
-        hex_label.config(text=f"Hex: {from_decimal(value, 16)}")
-    elif combo.get() == "Non-Decimal":
-        value = entry.get()  # Gets value from entry
-        base = int(base_combo.get())
-        result = to_decimal(value, int(base_combo.get()))
-        result_label.config(text=f"Decimal: {to_decimal(value, base)}")  # Shows result
+class NumbersTab:
+    def __init__(self, parent):
+
+        # Label + Entry for Input
+        ttk.Label(parent, text="Number:").grid(row=0, column=0)
+        self.entry = ttk.Entry(parent)
+        self.entry.grid(row=0, column=1)
+
+        # Dropdown menu
+        ttk.Label(parent, text="System:").grid(row=1, column=0)
+        self.combo = ttk.Combobox(parent, values=["Decimal", "Non-Decimal"])
+        self.combo.grid(row=1, column=1)
+
+        ttk.Label(parent, text="Base").grid(row=2, column=0)
+        self.base_combo = ttk.Combobox(parent, values=["2", "8", "16"])
+        self.base_combo.grid(row=2, column=1)
+
+        ttk.Button(parent, text="convert", command=self.numbers_click).grid(row=3, column=1)
+
+        # Label for result
+        self.result_label = ttk.Label(parent, text="", justify="left")
+        self.result_label.grid(row=4, column=1)
+        self.bin_label = ttk.Label(parent, text="", justify="left")
+        self.bin_label.grid(row=5, column=1)
+        self.oct_label = ttk.Label(parent, text="", justify="left")
+        self.oct_label.grid(row=6, column=1)
+        self.hex_label = ttk.Label(parent, text="", justify="left")
+        self.hex_label.grid(row=7, column=1)
+
+    # Button calls function
+    def numbers_click(self):
+        self.result_label.config(text="")
+        self.bin_label.config(text="")
+        self.oct_label.config(text="")
+        self.hex_label.config(text="")
+        if self.combo.get() == "Decimal":
+            value = int(self.entry.get())
+            self.bin_label.config(text=f"Binär: {from_decimal(value, 2)}")
+            self.oct_label.config(text=f"Oktal: {from_decimal(value, 8)}")
+            self.hex_label.config(text=f"Hex: {from_decimal(value, 16)}")
+        elif self.combo.get() == "Non-Decimal":
+            value = self.entry.get()  # Gets value from entry
+            base = int(self.base_combo.get())
+            result = to_decimal(value, int(self.base_combo.get()))
+            self.result_label.config(text=f"Decimal: {to_decimal(value, base)}")  # Shows result
 
 
-ttk.Button(numbers_tab, text="convert", command=on_click).grid(row=3, column=1)
+class StorageTab:
+    def __init__(self, parent):
 
-# Label for result
-result_label = ttk.Label(numbers_tab, text="", justify="left")
-result_label.grid(row=4, column=1)
-bin_label = ttk.Label(numbers_tab, text="", justify="left")
-bin_label.grid(row=5, column=1)
-oct_label = ttk.Label(numbers_tab, text="", justify="left")
-oct_label.grid(row=6, column=1)
-hex_label = ttk.Label(numbers_tab, text="", justify="left")
-hex_label.grid(row=7, column=1)
+        ttk.Label(parent, text="Input number:").grid(row=0, column=0)
+        self.entry = ttk.Entry(parent)
+        self.entry.grid(row=0, column=1)
 
-root.mainloop()
+        ttk.Label(parent, text="Base:").grid(row=1, column=0)
+        self.combo = ttk.Combobox(parent, values=["2", "10", "16"])
+        self.combo.grid(row=1, column=1)
+
+        ttk.Label(parent, text="Output number:").grid(row=3, column=0)
+        self.entry = ttk.Entry(parent)
+        self.entry.grid(row=3, column=1)
+
+if __name__ == "__main__":
+    app = App_window()
+    app.mainloop()
